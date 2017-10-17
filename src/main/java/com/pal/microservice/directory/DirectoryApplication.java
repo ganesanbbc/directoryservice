357a3d872e02a60655e2ac7d00bdcd467f7b0b53
ganesanbbc <sugan1983>
2017-10-17 15:52:11 +0530

    Refactor

--- src/main/java/com/pal/microservice/directory/DirectoryApplication.java
+++ src/main/java/com/pal/microservice/directory/DirectoryApplication.java
@@ -1,6 +1,7 @@
 package com.pal.microservice.directory;
 
 import com.pal.microservice.directory.model.Directory;
+import com.pal.microservice.directory.model.Person;
 import com.pal.microservice.directory.repository.DirectoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,18 +19,18 @@ public class DirectoryApplication {
         SpringApplication.run(DirectoryApplication.class, args);
     }
 
-//    @Bean
-//    CommandLineRunner addPersons(DirectoryRepository repo) {
-//        return args -> {
-//            List<Directory.Person> people = asList(
-//                    new Directory.Person("John", "Doe", "john@email.com",
-//                            "Denver,CO", "1-800-9012345"),
-//                    new Directory.Person("Carl", "Doe", "carl@email.com",
-//                            "Albuquerque,NM", "404-2346781"),
-//                    new Directory.Person("Trish", "Doe", "trish@email.com",
-//                            "Miamu,FL", "305-4562789")
-//            );
-//            repo.save(new Directory("my-directory", people));
-//        };
-//    }
+    @Bean
+    CommandLineRunner addPersons(DirectoryRepository repo) {
+        return args -> {
+            List<Person> people = asList(
+                    new Person("John", "Doe", "john@email.com",
+                            "Denver,CO", "1-800-9012345"),
+                    new Person("Carl", "Doe", "carl@email.com",
+                            "Albuquerque,NM", "404-2346781"),
+                    new Person("Trish", "Doe", "trish@email.com",
+                            "Miamu,FL", "305-4562789")
+            );
+            repo.save(new Directory("my-directory", people));
+        };
+    }
 }
