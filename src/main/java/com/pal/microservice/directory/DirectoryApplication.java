f05abaaf7a97777bbc717839554e9b5ebe1f50ff
ganesanbbc <sugan1983>
2017-10-17 15:50:10 +0530

    Refactor

--- src/main/java/com/pal/microservice/directory/DirectoryApplication.java
+++ src/main/java/com/pal/microservice/directory/DirectoryApplication.java
@@ -18,18 +18,18 @@ public class DirectoryApplication {
         SpringApplication.run(DirectoryApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner addPersons(DirectoryRepository repo) {
-        return args -> {
-            List<Directory.Person> people = asList(
-                    new Directory.Person("John", "Doe", "john@email.com",
-                            "Denver,CO", "1-800-9012345"),
-                    new Directory.Person("Carl", "Doe", "carl@email.com",
-                            "Albuquerque,NM", "404-2346781"),
-                    new Directory.Person("Trish", "Doe", "trish@email.com",
-                            "Miamu,FL", "305-4562789")
-            );
-            repo.save(new Directory("my-directory", people));
-        };
-    }
+//    @Bean
+//    CommandLineRunner addPersons(DirectoryRepository repo) {
+//        return args -> {
+//            List<Directory.Person> people = asList(
+//                    new Directory.Person("John", "Doe", "john@email.com",
+//                            "Denver,CO", "1-800-9012345"),
+//                    new Directory.Person("Carl", "Doe", "carl@email.com",
+//                            "Albuquerque,NM", "404-2346781"),
+//                    new Directory.Person("Trish", "Doe", "trish@email.com",
+//                            "Miamu,FL", "305-4562789")
+//            );
+//            repo.save(new Directory("my-directory", people));
+//        };
+//    }
 }
